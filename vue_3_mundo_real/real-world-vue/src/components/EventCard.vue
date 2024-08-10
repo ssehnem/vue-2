bb90e5a5aaf952721491852ef1b3ffa2ddbe3ff4
Sara Sehnem <sarasehnem84@gmail.com>
2024-08-09 22:00:49 -0300

    Initial commit

---
+++ vue_3_mundo_real/real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <RouterLink
+    class="event-link"
+    :to="{ name: 'event-details', params: { id: event.id } }"
+  >
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped>
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+</style>
---
+++ vue_3_mundo_real/real-world-vue/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventDetailsView from '../views/EventDetailsView.vue'
+import AboutView from '../views/AboutView.vue'
+import EventListView from '../views/EventListView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventListView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView,
+    }
+
+  ],
+})
+
+export default router
